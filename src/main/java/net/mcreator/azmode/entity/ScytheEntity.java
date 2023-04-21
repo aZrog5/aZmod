be874d387b0f5f21f1392f7701eda973149e1a5c
Lilian222 <lilian.lhotel222@gmail.com>
2023-04-21 21:37:50 +0200

    Creation de la procedure de spawn de Scythe

--- src/main/java/net/mcreator/azmode/entity/ScytheEntity.java
+++ src/main/java/net/mcreator/azmode/entity/ScytheEntity.java
@@ -15,30 +15,21 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.network.PlayMessages;
 import net.minecraftforge.network.NetworkHooks;
 
-import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.monster.Blaze;
 import net.minecraft.world.entity.ai.navigation.PathNavigation;
 import net.minecraft.world.entity.ai.navigation.FlyingPathNavigation;
-import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
-import net.minecraft.world.entity.ai.goal.RandomStrollGoal;
-import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
-import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
-import net.minecraft.world.entity.ai.goal.FloatGoal;
 import net.minecraft.world.entity.ai.control.FlyingMoveControl;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
-import net.minecraft.world.entity.SpawnPlacements;
 import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.MobType;
 import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.Difficulty;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.syncher.SynchedEntityData;
@@ -49,7 +40,7 @@ import net.minecraft.core.BlockPos;
 
 import net.mcreator.azmode.init.AzmodeModEntities;
 
-public class ScytheEntity extends Monster implements IAnimatable {
+public class ScytheEntity extends Blaze implements IAnimatable {
 	public static final EntityDataAccessor<Boolean> SHOOT = SynchedEntityData.defineId(ScytheEntity.class, EntityDataSerializers.BOOLEAN);
 	public static final EntityDataAccessor<String> ANIMATION = SynchedEntityData.defineId(ScytheEntity.class, EntityDataSerializers.STRING);
 	public static final EntityDataAccessor<String> TEXTURE = SynchedEntityData.defineId(ScytheEntity.class, EntityDataSerializers.STRING);
@@ -66,7 +57,7 @@ public class ScytheEntity extends Monster implements IAnimatable {
 	public ScytheEntity(EntityType<ScytheEntity> type, Level world) {
 		super(type, world);
 		xpReward = 0;
-		setNoAi(false);
+		setNoAi(true);
 		this.moveControl = new FlyingMoveControl(this, 10, true);
 	}
 
@@ -97,21 +88,6 @@ public class ScytheEntity extends Monster implements IAnimatable {
 	}
 
 	@Override
-	protected void registerGoals() {
-		super.registerGoals();
-		this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.2, false) {
-			@Override
-			protected double getAttackReachSqr(LivingEntity entity) {
-				return this.mob.getBbWidth() * this.mob.getBbWidth() + entity.getBbWidth();
-			}
-		});
-		this.goalSelector.addGoal(2, new RandomStrollGoal(this, 1));
-		this.targetSelector.addGoal(3, new HurtByTargetGoal(this));
-		this.goalSelector.addGoal(4, new RandomLookAroundGoal(this));
-		this.goalSelector.addGoal(5, new FloatGoal(this));
-	}
-
-	@Override
 	public MobType getMobType() {
 		return MobType.UNDEAD;
 	}
@@ -134,6 +110,8 @@ public class ScytheEntity extends Monster implements IAnimatable {
 	@Override
 	public boolean hurt(DamageSource source, float amount) {
 		if (source == DamageSource.FALL)
+			return false;
+		if (source == DamageSource.LIGHTNING_BOLT)
 			return false;
 		if (source == DamageSource.WITHER)
 			return false;
@@ -162,14 +140,14 @@ public class ScytheEntity extends Monster implements IAnimatable {
 		super.setNoGravity(true);
 	}
 
+	@Override
 	public void aiStep() {
 		super.aiStep();
+		this.updateSwingTime();
 		this.setNoGravity(true);
 	}
 
 	public static void init() {
-		SpawnPlacements.register(AzmodeModEntities.SCYTHE.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-				(entityType, world, reason, pos, random) -> (world.getDifficulty() != Difficulty.PEACEFUL && Monster.isDarkEnoughToSpawn(world, pos, random) && Mob.checkMobSpawnRules(entityType, world, reason, pos, random)));
 	}
 
 	public static AttributeSupplier.Builder createAttributes() {
